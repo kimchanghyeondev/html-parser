81297c645a02c6f10477bb1fe831b721105f4b4b
kimchanghyeondev <kimchanghyeon1201@gmail.com>
2022-03-12 13:14:56 +0900

    feat: 1.api documentation

--- src/main/java/com/example/demo/config/ClientWebConfig.java
+++ src/main/java/com/example/demo/config/ClientWebConfig.java
@@ -4,13 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class ClientWebConfig  implements WebMvcConfigurer {
+public class ClientWebConfig implements WebMvcConfigurer {
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 }
+
+
